e08af26f970a018c4fbc0c0e561ae6da5e68b536
elchroy <chijioke.elisha-wigwe@andela.com>
2017-10-21 21:03:52 +0100

    test on diabetes dataset

---
+++ diabetes.py
@@ -0,0 +1,28 @@
+"""
+Artificial Neural Networks - Classification (Appendicitis)
+"""
+from network import *
+from numpy import genfromtxt
+
+# instantiate the network
+num_of_features = 8
+net = Network([num_of_features, 1000, 1])
+
+# Prepare the training data
+train = genfromtxt('diabetes_train.csv', delimiter=",")
+test = genfromtxt('diabetes_test.csv', delimiter=",")
+
+x_train = train[:, 0:num_of_features]
+x_train = [ [ (i - xt.min())/(xt.max() - xt.min()) for i in xt ] for xt in x_train]
+y_train = train[:, [num_of_features]]
+
+training_data = zip(x_train, y_train)
+
+# Train the neural network
+net.train(training_data, mini_batch_size=17, lr=0.03, check=10)
+
+# Accuracy: 551.0/765 => 72.0261437908%
+# After 640 in 973.979948044 seconds
+# 
+# Accuracy: 555.0/765 => 72.5490196078%
+# After 780 in 1163.19382906 seconds
